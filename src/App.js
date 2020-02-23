15c80ade6f16f30655c492f6d0912c1516afe4ad
amoghwagh <amoghwagh@gmail.com>
2020-02-23 22:32:13 +0530

    Added a global context api provider

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import { Balance } from "./components/Balance";
 import { IncomeExpenses } from "./components/IncomeExpenses";
 import { TransactionList } from "./components/TransactionList";
 import { AddTransaction } from "./components/AddTransaction";
-
+import { GlobalProvider } from "./context/GlobalState";
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <div className="container">
-          <Balance />
-          <IncomeExpenses />
-          <TransactionList />
-          <AddTransaction />
+      <GlobalProvider>
+        <div>
+          <Header />
+          <div className="container">
+            <Balance />
+            <IncomeExpenses />
+            <TransactionList />
+            <AddTransaction />
+          </div>
         </div>
-      </div>
+      </GlobalProvider>
     );
   }
 }
---
+++ src/context/GlobalState.js
@@ -0,0 +1,23 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+// Initial State
+const initialState = {
+  transactions: [
+    { id: 1, text: "Flower", amount: -20 },
+    { id: 2, text: "Salary", amount: 300 }
+  ]
+};
+
+// Create Context
+export const GlobalContext = createContext(initialState);
+
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
