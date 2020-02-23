f342b313ebb39265d4efde6a615b3c76ba03060c
Amogh Wagh <amoghwagh@gmail.com>
2020-02-23 20:09:03 +0530

    Added Transaction List component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
 import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+
 class App extends Component {
   render() {
     return (
@@ -11,6 +13,7 @@ class App extends Component {
         <div className="container">
           <Balance />
           <IncomeExpenses />
+          <TransactionList />
         </div>
       </div>
     );
