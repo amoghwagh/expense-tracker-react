a329e5bc9d5134581a4ba83c0552550be30bfeed
Amogh Wagh <amoghwagh@gmail.com>
2020-02-23 19:40:48 +0530

    Added Header and Balance components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
-
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
 class App extends Component {
   render() {
-    return <div>Hello World! </div>;
+    return (
+      <div>
+        <Header />
+        <div className="container">
+          <Balance />
+        </div>
+      </div>
+    );
   }
 }
 
