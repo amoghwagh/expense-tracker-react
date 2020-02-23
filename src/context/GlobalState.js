58b1a7c297b5cc4eff20858eaeabcd57b1500549
amoghwagh <amoghwagh@gmail.com>
2020-02-23 23:51:37 +0530

    Added addTransaction action and reducer

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -22,9 +22,20 @@ export const GlobalProvider = ({ children }) => {
       payload: id
     });
   }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction
+    });
+  }
   return (
     <GlobalContext.Provider
-      value={{ transactions: state.transactions, deleteTransaction }}
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
+      }}
     >
       {children}
     </GlobalContext.Provider>
